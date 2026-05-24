db5af98ebf96986de58d0d008adb7d3ee4550985
Ravimanikonda <110715223+Ravimanikonda@users.noreply.github.com>
2026-05-24 09:26:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -7,10 +6,10 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 URL = "https://in.bookmyshow.com/movies/hyderabad/peddi/ET00439772"
 
-TARGET_THEATRE = "Sudarshan"
+TARGET_THEATRE = "peddi"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    "User-Agent": "Mozilla/5.0"
 }
 
 response = requests.get(URL, headers=headers)
@@ -23,7 +22,7 @@ if TARGET_THEATRE.lower() in page_text:
 
     data = {
         "chat_id": CHAT_ID,
-        "text": f"🔥 PEDDI BOOKINGS OPENED at {TARGET_THEATRE} 🔥"
+        "text": "🔥 PEDDI ALERT WORKING 🔥"
     }
 
     requests.post(telegram_url, data=data)
@@ -31,4 +30,4 @@ if TARGET_THEATRE.lower() in page_text:
     print("Alert Sent")
 
 else:
-    print("Bookings Not Open Yet")
+    print("No Alert")
